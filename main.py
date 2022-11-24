2842ce4641ec99c8f5399fd438b4a10e3edd2c9f
Ana Leite <116755367+negaanaleite@users.noreply.github.com>
2022-11-24 14:46:02 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+#-------- A T I V I D A D E S-----------------
+
+# 01- Descrever o problema
+# def minha_funcao():
+# 	for i in range(1,21):
+# 		if i ==20:
+# 			print("Você entendeu")
+# minha_funcao()			
+
+# 02- Reproduzir o  Bug
+# from random import randint
+# dados_imag = ["❶", "❷", "❸", "❹", "❺", "❻"]
+# dados_num = randint(0, 5)
+# print(dados_imag[dados_num])
+
+# 03- Jogar computador
+# ano = int(input("Qual é o seu ano de nascimento?"))
+# if ano > 1980 and ano < 1994:
+#   print("Você é um milenar.")
+# elif ano >= 1994:
+#   print("Você é geração Z")
+
+#04- Corrigir os erros
+# idade = int(input("Quantos anos você tem? "))
+# if idade >= 18:
+# 	print(f"Você tem {idade}, por isso já pode.")
+# else:
+# 	if idade < 18:
+# 		print(" Você só pode dirigir com 18 anos")
+
+# 05- A impressão é sua amiga
+# pagina = 0
+# plvr_por_pag = 0
+# pagina = int(input("Numero de Páginas: "))
+# plvr_por_pag = int(input("Número de palavras por página:"))
+# total_plvr = pagina * plvr_por_pag
+# print(f"Página = {pagina}")
+# print(f"Palavras por Página = {plvr_por_pag} ")
+# print(f"Total = {total_plvr}")
+
+# 06- Use a Debugger
+# def mutate(a_list):
+#   b_list = []
+#   for item in a_list:
+#     new_item = item * 2
+#   b_list.append(new_item)
+#   print(b_list)
+
+# mutate([1,2,3,5,8,13])
